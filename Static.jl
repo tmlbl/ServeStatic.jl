23ba35941aa9530394753c9f732389cdc51dc2e6
Tim Lebel <tim.lebel@gmail.com>
2015-05-27 00:46:56 -0700

    Edits

--- Static.jl
+++ Static.jl
@@ -9,7 +9,7 @@ export ServeStatic
 path_in_dir(p::String, d::String) = length(p) > length(d) && p[1:length(d)] == d
 
 function strip_trailing_slash(p::String)
-  if p[length(p)] == '/'
+  if p[length(p)] == '/' && p != "/"
     return p[1:length(p)-1]
   end
   p
@@ -20,18 +20,19 @@ function ServeStatic(root::String)
     m = match(r"^/+(.*)$", req.state[:resource])
     if m != nothing
       path = normpath(root, m.captures[1])
-      # Protect against dir-escaping
+#     # Protect against dir-escaping
       if !path_in_dir(path, root)
         return respond(req, Response(400)) # Bad Request
       end
+      path = strip_trailing_slash(path)
       # Serve the requested file, if it exists
       if isfile(path)
         res.data = readall(path)
         return respond(req, res)
       end
       # Serve index.html if a request is made to the root
-      if (strip_trailing_slash(path) == normpath(root))
-        res.data = readall(strip_trailing_slash(path) * "/index.html")
+      if (path == normpath(root))
+        res.data = readall(path * "/index.html")
         return respond(req, res)
       end
       req, res
--- test/test.jl
+++ test/test.jl
@@ -6,3 +6,10 @@ response = get("http://0.0.0.0:8084")
 
 @test response.status == 200
 @test response.data == "<body>hello</body>"
+
+
+println("It should serve a file")
+response = get("http://0.0.0.0:8084/script.js")
+
+@test response.status == 200
+@test response.data == "alert;\n"
